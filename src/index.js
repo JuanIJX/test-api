1a38c1e3b149c0699b3ddfbf912edcf8f9094aa0
JuanIJX <srwe-@hotmail.com>
2023-01-25 02:04:34 +0100

    Arreglar console.log(ip)

--- src/index.js
+++ src/index.js
@@ -66,7 +66,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use('*', async function (req, res) {
 	console.log("===============");
-	console.log("IP :"+req.ip.substring(req.ip.lastIndexOf(":")+1));
+	console.log("IP: "+req.ip.substring(req.ip.lastIndexOf(":")+1));
 	console.log("req.originalUrl");
 	console.log(req.originalUrl);
 	console.log("req.headers");
